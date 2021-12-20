140530a51878d9386d292cf0c679bc0e54f9cb39
tsuchiya-kai <yukaigangan@gmail.com>
2021-12-21 01:10:29 +0900

    いったんコミット

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ type User = {
 };
 
 const users: User[] = [
-  { id: 1, name: "User1", email: "user1@test.local" },
+  { id: 1, name: "User10000000000000", email: "user1@test.local" },
   { id: 2, name: "User2", email: "user2@test.local" },
   { id: 3, name: "User3", email: "user3@test.local" },
 ];
